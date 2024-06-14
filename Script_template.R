5e0db5f4de9e77aa3b004c0851faff7669bde450
pottj <pott.ne.ja@gmail.com>
2024-06-14 20:43:18 +0200

    add template script

---
+++ Script_template.R
@@ -0,0 +1,30 @@
+#' ---
+#' title: ""
+#' subtitle: "MR of SH on GE"
+#' author: "Janne Pott"
+#' date: "Last compiled on `r format(Sys.time(), '%d %B, %Y')`"
+#' output:
+#'   html_document:
+#'     toc: true
+#'     number_sections: true
+#'     toc_float: true
+#'     code_folding: show
+#' ---
+#'
+#' # Introduction ####
+#' ***
+#' 
+#' 
+#' # Initialize ####
+#' ***
+rm(list = ls())
+time0<-Sys.time()
+
+source("../SourceFile_forostar.R")
+.libPaths()
+
+#' # Session Info ####
+#' ***
+sessionInfo()
+message("\nTOTAL TIME : " ,round(difftime(Sys.time(),time0,units = "mins"),3)," minutes")
+
